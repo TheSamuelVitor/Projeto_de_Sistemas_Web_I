9341e5773d7356c94545c7935020def73757bcd6
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-10-10 09:06:07 -0300

    feat: add style on submit[

--- projetoPrestacao/script.js
+++ projetoPrestacao/script.js
@@ -1,6 +1,12 @@
 var qtdprestacao = document.getElementById("qtdPrestacao");
 var valor = document.getElementById("valor");
 var divPrestacoes = document.getElementById("prestacoes")
+var mensagem = document.getElementById("mensagem")
+
+if (qtdprestacao.value > 5) {
+  console.log(qtdprestacao.value)
+  mensagem.innerText = "Aviso! Quantidade de prestacoes excedendo 5"
+}
 
 function calcula() {
   console.log(qtdprestacao.value)
@@ -8,7 +14,7 @@ function calcula() {
 
   for (let index = 0; index < qtdprestacao.value; index++) {
     const prestacao = document.createElement("p")
-    prestacao.innerText = `${qtdprestacao.value} Parcelas no valor de ${(valor.value/qtdprestacao.value).toFixed(2)}`
+    prestacao.innerText = `Parcelas no valor de ${(valor.value/qtdprestacao.value).toFixed(2)}`
     document.body.appendChild(prestacao)
   }
 }
