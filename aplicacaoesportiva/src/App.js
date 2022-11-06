c168bccbd466e81a6e111687c3c19ee75708d116
TheSamuelVitor <thesamuelvitor.py@gmail.com>
2022-11-06 20:47:05 -0300

    :tada: init: react app for soccer

---
+++ aplicacaoesportiva/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Titulo from "./components/teste.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Titulo></Titulo>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ aplicacaoesportiva/src/components/teste.js
@@ -0,0 +1,6 @@
+function Titulo() {
+  const titulo = <h1>Aplicacao Esportiva</h1>;
+  return titulo;
+}
+
+export default Titulo;
