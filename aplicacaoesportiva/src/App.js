1c91cf01b6daf65335a0ad95ec1205ed38a55793
TheSamuelVitor <thesamuelvitor.py@gmail.com>
2022-11-13 14:25:09 -0300

    feat: connection with api

--- aplicacaoesportiva/src/App.js
+++ aplicacaoesportiva/src/App.js
@@ -2,19 +2,13 @@ import client from "./api/connection";
 import "./App.css";
 import Titulo from "./components/titulo";
 
-
 function App() {
+  const campeonatos = [];
 
-  const campeonatos = []
+  client.get("v1/campeonatos").then((response) => {
+    campeonatos.push(response.data);
+  });
 
-  client.get("v1/campeonatos").then(
-    (response) => {
-      campeonatos.push(response.data)
-      console.log(response.data)
-    }
-  )
-
-  console.log(campeonatos)
   return (
     <div className="App">
       <header className="App-header">
