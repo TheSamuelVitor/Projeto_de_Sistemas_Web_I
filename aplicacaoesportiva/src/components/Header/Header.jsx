c8f7a6a6b2dc9b77254c69b8badc25369ecad666
rafaelcarvalho-git <rafaskyplay@gmail.com>
2022-11-15 22:16:53 -0300

    versao 1.0

--- aplicacaoesportiva/src/components/Header/Header.jsx
+++ aplicacaoesportiva/src/components/Header/Header.jsx
@@ -6,7 +6,6 @@ function Header() {
         <header className="text-center mt-4 mb-5">
             <GiSoccerField className="field-icon" />
             <h1 className="text-center">Football Matches</h1>
-            <p className="lead">Aplicação para visualizar partidas e placares de futebol do dia.</p>
         </header>                
     );
 }
--- aplicacaoesportiva/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-//import './Home.css'
-import { Link } from 'react-router-dom'
-import Championships from "../components/Championships/Championships"
-
-function Home() {
-    return <Link to="/matches"><Championships /></Link>         
-}
-
-export default Home;
